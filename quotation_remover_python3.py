5937b2b3227d05e2cb07dc09b70b52c41867146c
Heather Spencer <spencerhb@vcu.edu>
2017-02-17 11:20:46 -0500

    Add files via upload

---
+++ quotation_remover_python3.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+if len(sys.argv) <1:
+    #print "Usage: python %s requires input directory" % (sys.argv[0])
+    sys.exit()
+
+#dir_name=
+#infile= sys.argv[1]
+#outfile=infile.split(".")[0] + "_output.txt"
+dirs= os.listdir('.')
+
+
+##header=INFH.readline()
+##header= header.strip()
+
+
+
+
+for f in dirs:
+  if f.endswith(".txt"):
+      infile=f
+      outfile=f.split(".")[0] + "_output.txt"
+      INFH=open(f)
+      OFH=open(outfile, "w")
+      with open(f) as fh:
+          for line in fh:
+              line=line.replace('\"','')
+              
+              print(line,file=OFH, )
+
+      print("Output file=", outfile)
+
+      INFH.close()
+
